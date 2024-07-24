97b408b7818c87e2aec9a7c75be90d3202af1360
Chris Little <chris.little@sonnedix.com>
2024-07-24 16:48:58 +0100

    Add bucket policy

--- infra/s3_bucket/main.tf
+++ infra/s3_bucket/main.tf
@@ -1,3 +1,20 @@
 resource "aws_s3_bucket" "project_bucket" {
   bucket = "${var.project}-bucket-${var.aws_account_id}"
 }
+
+data "aws_iam_policy_document" "project_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.project_bucket.bucket}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "project_bucket_policy" {
+  bucket = aws_s3_bucket.project_bucket.bucket
+
+  policy = data.aws_iam_policy_document.project_bucket_policy.json
+}
